710d381d751b29f57cf427259512ed34be84d829
Balaji S. Srinivasan <balajis@stanford.edu>
2013-03-20 22:55:52 -0700

    Added response to HTTP requests.
    https://devcenter.heroku.com/articles/nodejs

--- demo.js
+++ demo.js
@@ -1,1 +1,12 @@
-console.log("First console log.");
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
